eba99b41f4b07c97b6ce29beb2f46d1040193fd1
Cameron Oliver <cameron.oliver@gmail.com>
2014-05-03 19:52:50 +1200

    Distutils configuration

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+
+setup(
+	name = "pypassage",
+	packages = ['pypassage'],
+	version = '1.0',
+	author = 'Cameron Oliver',
+	author_email = 'cameron.oliver@gmail.com',
+	url = 'https://github.com/col16/pypassage',
+	description = 'Small Python module for working with bible passages/references',
+	long_description = "",
+	classifiers = [
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 2.7",
+		"License :: OSI Approved :: ISC License (ISCL)",
+		"Operating System :: OS Independent",
+		"Development Status :: 3 - Alpha",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development :: Libraries",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Topic :: Religion"
+		],
+)
